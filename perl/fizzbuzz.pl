d816ffe8b7e363e1b01e9ddb34f523c4c44a361f
Jacob Prystowsky <jprystowsky@ohio-cpa.com>
2013-05-06 14:33:39 -0400

    Adding FizzBuzz for Perl.

---
+++ perl/fizzbuzz.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+for(1..100) {
+	if ($_ % 15 == 0) {
+		print "FizzBuzz\n";
+	} elsif ($_ % 5 == 0) {
+		print "Buzz\n";
+	} elsif ($_ % 3 == 0) {
+		print "Fizz\n";
+	} else {
+		print "$_\n";
+	}
+}
